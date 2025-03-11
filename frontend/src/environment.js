61117d8c71d77d03109e5912560ea49dc815c085
Gaytripiwal <piwalgaytri1@gmail.com>
2025-03-11 09:37:16 +0530

    Add Files

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,9 +1,12 @@
 // let IS_PROD = true;
 let IS_PROD = false;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+   // "https://apnacollegebackend.onrender.com" :
+   "https://apnavideocallbackend-4psg.onrender.com"
 
-    "http://localhost:8000"
+    :"http://localhost:1000"
+    // const API_BASE_URL = "http://localhost:10000/api/v1";
+
 
 
 export default server;
