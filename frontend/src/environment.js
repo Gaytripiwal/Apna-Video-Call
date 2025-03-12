cd34234d6952e7a4e484a4d3dd592e6183aad8cb
Gaytripiwal <piwalgaytri1@gmail.com>
2025-03-12 11:30:54 +0530

    Add Files

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,12 +1,10 @@
-// let IS_PROD = true;
-let IS_PROD = false;
+
+let IS_PROD = true;
 const server = IS_PROD ?
-   // "https://apnacollegebackend.onrender.com" :
-   "https://apnavideocallbackend-4psg.onrender.com"
+    
+    "https://apnavideocallbackend-4psg.onrender.com":
 
-    :"http://localhost:8000"
-    // const API_BASE_URL = "http://localhost:10000/api/v1";
-
+    "http://localhost:8000"
 
 
 export default server;
