7cb1719ad4d0b4400e28f3d7fa4bea8ea1b85af9
Gaytripiwal <piwalgaytri1@gmail.com>
2025-03-12 12:16:46 +0530

    Add Files

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
 
 let IS_PROD = true;
 const server = IS_PROD ?
-    
-    "https://apnavideocallbackend-4psg.onrender.com":
+
+    "https://meetrtc-backend.onrender.com":
 
     "http://localhost:8000"
 
